f85eb916051e2d5ac8394b4f02f9c21b7cb7d3d9
Victor <moreau.totor@gmail.com>
2025-02-22 16:01:36 +0100

    feat(setup-monorepo): apply tailwind config to libs folder

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -5,6 +5,7 @@ const { join } = require('path');
 module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, './../../libs/**/!(*.stories|*.spec).{ts,html}'), // Include libs/
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
